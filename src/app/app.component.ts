7ec6b4c3e4191032ce4a01b15ab056609386a7f4
siddhi vinayaka <svinayaka290489@gmail.com>
2019-11-15 08:36:54 +0530

    update media queries for list view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,8 @@ interface ICode {
 })
 export class AppComponent {
   public title = 'Bytemark India news';
-  public countrySelected: ICode = {text: 'India', code: 'in'};
-  public categorySelected: ICode = { text: "Entertainment", code: 'entertainment' };
   public country: Array<ICode> = [{text: 'India', code: 'in'}];
+  public countrySelected: ICode = this.country[0];
   public category: Array<{text: string, code: string}> = [
     { text: 'Business', code: 'business' },
     { text: 'Entertainment', code: 'entertainment' },
@@ -25,6 +24,7 @@ export class AppComponent {
     { text: 'Sports', code: 'sports' },
     { text: 'Technology', code: 'technology' }
   ];
+  public categorySelected: ICode = this.category[0];
 
   constructor(private service: LayoutresolverService) {
     this.service.formUrl(this.countrySelected.code, this.categorySelected.code);
